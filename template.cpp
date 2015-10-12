070b70b066e8b3448c736dcb66be1e46013b3cad
Saurabh Joshi <saurabh.joshi.cse@gmail.com>
2015-10-12 03:56:18 -0700

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define LL long long int
+#define M 1000000007
+#define reset(a) memset(a,0,sizeof(a))
+#define rep(i,j,k) for(i=j;i<=k;++i)
+#define per(i,j,k) for(i=j;i>=k;--i)
+#define print(a,start,end) for(i=start;i<=end;++i) cout<<a[i];
+#define endl "\n"
+using namespace std;
+int main()
+    {
+        ios_base::sync_with_stdio(0);
+    }
