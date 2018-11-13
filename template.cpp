2ea1e104a177a2f242089882923a56b09c265976
Saurabh Joshi <saurabh.joshi@iiitdmj.ac.in>
2018-11-13 17:12:33 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,16 +1,17 @@
+/*
+Saurabh Joshi
+IIIT Jabalpur
+*/
 #include<bits/stdc++.h>
 #define LL long long int
 #define M 1000000007
-#define reset(a) memset(a,0,sizeof(a))
-#define rep(i,j,k) for(i=j;i<=k;++i)
-#define per(i,j,k) for(i=j;i>=k;--i)
-#define print(a,start,end) for(i=start;i<=end;++i) cout<<a[i];
 #define endl "\n"
+#define eps 0.00000001
 LL pow(LL a,LL b,LL m){LL x=1,y=a;while(b > 0){if(b%2 == 1){x=(x*y);if(x>m) x%=m;}y = (y*y);if(y>m) y%=m;b /= 2;}return x%m;}
 LL gcd(LL a,LL b){if(b==0) return a; else return gcd(b,a%b);}
+LL gen(LL start,LL end){LL diff = end-start;LL temp = rand()%start;return temp+diff;}
 using namespace std;
 int main()
     {
         ios_base::sync_with_stdio(0);
-        LL L,R,K,N;
     }
